9beab55c848bbff2d1ca52b7591d31252e7c1c05
mahzar1783 <mazhar.hayat@Menaa.local>
2021-08-22 17:34:46 +0400

    "adding routing, adding shoping add header"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Header from './components/header/header.component';
 import HomePage from './pages/home/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 
 function App() {
   return (
-    <div className="App">
-     <HomePage></HomePage>
+    <div>
+    <BrowserRouter>
+    <Header/>
+        <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
